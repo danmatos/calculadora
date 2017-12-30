cdf75e55c0d8f06a6f01d9c0a9adf7974f4c379b
Daniel Matos <danielmatos.it@gmail.com>
2017-12-30 14:50:30 -0200

    Implements unit tests on calculadora service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {CalculadoraModule} from './calculadora';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        CalculadoraModule
       ],
     }).compileComponents();
   }));
--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,6 +1,7 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import {CalculadoraComponent} from './calculadora.component';
+import {CalculadoraService} from '../services';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -8,7 +9,12 @@ describe('CalculadoraComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CalculadoraComponent]
+      declarations: [
+        CalculadoraComponent
+      ],
+      providers: [
+        CalculadoraService
+      ]
     })
       .compileComponents();
   }));
@@ -22,4 +28,40 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      const soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    })
+  );
+
+  it('deve garantir que 1 / 4 = 0.25',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      const divisao = service.calcular(1, 4, CalculadoraService.DIVISAO);
+      expect(divisao).toEqual(0.25);
+    })
+  );
+
+  it('deve garantir que 1 - 4 = 3',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      const subtracao = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect(subtracao).toEqual(-3);
+    })
+  );
+
+  it('deve garantir 1 * 4 = 4',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      const multiplicacao = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+      expect(multiplicacao).toEqual(4);
+    })
+  );
+
+  it('deve retornar 0 para operação invalida',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      const operacaoInvalida = service.calcular(1, 4, '%');
+      expect(operacaoInvalida).toEqual(0);
+    })
+  );
+
 });
